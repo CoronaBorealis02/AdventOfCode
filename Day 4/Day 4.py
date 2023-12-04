77400be2e923f1de1e6810d13c9480a1e811f2ff
CoronaBorealis02 <github@archieburdick.co.uk>
2023-12-04 17:21:51 +0000

    solution to part 1

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -1,4 +1,4 @@
-f = open("C:/Users/up2068237/source/repos/CoronaBorealis02/AdventOfCode/Day 4/input.txt", "r")
+f = open("Day 4/input.txt", "r")
 
 cardlist = f.read().split("\n")
 cards = []
@@ -12,13 +12,14 @@ for i in cards:
 	winningnums.append(i.split(" | ")[0])
 	yournums.append(i.split(" | ")[1])
 
-
+total = 0
 for i in range(len(winningnums)):
 	points = 0
 	wincount = 0
 	winnum = winningnums[i].split(" ")
 	yournum = yournums[i].split(" ")
-	
+	winnum = [x for x in winnum if x.strip()]
+	yournum = [x for x in yournum if x.strip()]
 	for ii in yournum:
 		if ii in winnum:
 			wincount += 1
@@ -26,4 +27,5 @@ for i in range(len(winningnums)):
 		points = 1
 		for i in range(wincount - 1):
 			points += points
-	print(points)
+	total += points
+print(total)
